66b8efa344b1814e3abd6174ccc74c35cfbc2c52
Kevin Yeung <github@yeung.online>
2018-05-30 16:55:40 +0800

    Find similar suburbs in terms of incomes

--- code/suburb_lookup_functions.R
+++ code/suburb_lookup_functions.R
@@ -18,5 +18,13 @@ lookup_suburb_area <- function(suburb_name) {
   area
 }
 
+lookup_suburb_name_by_ssc_code <- function(code) {
+  code <- as.character(code)
+  if (startsWith(code, "SSC")) {
+    code <- substr(code, 4, 8)
+  }
+  unique(ssc_codes[ssc_codes$SSC_CODE_2016 == code, "SSC_NAME_2016"])
+}
+
 library(dplyr)
 ssc_codes <- read.csv("./census2016/SSC_2016_AUST.csv") %>% mutate(SSC_NAME_2016 = toupper(SSC_NAME_2016))
